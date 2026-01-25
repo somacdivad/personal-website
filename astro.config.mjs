f04775ccab51d6fa0509609da9d98ccca0fd5895
David Amos <somacdivad@gmail.com>
2026-01-25 11:46:06 -0600

    use daisyUI navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://davidamoswrites.com',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [mdx(), sitemap(), icon()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'David Amos | Author & Technical Writer';
+export const SITE_DESCRIPTION = 'Don\'t know who David Amos is? Me either, but this is my best attempt at figuring it out.';
