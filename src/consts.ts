85ee9022b8b7f8b7f5eaec9c52ec5aaf26f28ccb
David Amos <somacdivad@gmail.com>
2026-01-25 12:35:35 -0600

    hero and footer

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,20 @@
 
 export const SITE_TITLE = 'David Amos | Author & Technical Writer';
 export const SITE_DESCRIPTION = 'Don\'t know who David Amos is? Me either, but this is my best attempt at figuring it out.';
+export const SOCIAL_LINKS = [
+    {
+        name: 'Mastodon',
+        href: 'https://hachyderm.io/@somacdivad',
+        icon: 'logos/mastodon'
+    },
+    {
+        name: 'BlueSky',
+        href: 'https://bsky.app/profile/somacdivad.bsky.social',
+        icon: 'logos/bluesky'
+    },
+    {
+        name: 'GitHub',
+        href: 'https://github.com/somacdivad',
+        icon: 'logos/github'
+    },
+]
