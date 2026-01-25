6a8f1e04e0cfb6d0e1a012eb685f2bd64b5c509a
David Amos <somacdivad@gmail.com>
2026-01-25 17:45:38 -0600

    Initial setup (#1)
    * use daisyUI navbar
    
    * hero and footer
    
    * theme selector
    
    * update home page
    
    * add philosophy page and remove blog posts
    
    * favicon and fonts
    
    * move image to public
    
    * move icon to public
    
    * fix icon url
    
    * fix url again

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,27 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'David Amos | Author & Technical Writer';
+export const SITE_DESCRIPTION = 'Don\'t know who David Amos is? Me either, but this is my best attempt at figuring it out.';
+export const SOCIAL_LINKS = [
+    {
+        name: 'Medium',
+        href: 'https://somacdivad.medium.com/',
+        icon: 'logos/medium'
+    },
+    {
+        name: 'Mastodon',
+        href: 'https://hachyderm.io/@somacdivad',
+        icon: 'logos/mastodon'
+    },
+    {
+        name: 'BlueSky',
+        href: 'https://bsky.app/profile/somacdivad.bsky.social',
+        icon: 'logos/bluesky'
+    },
+    {
+        name: 'GitHub',
+        href: 'https://github.com/somacdivad',
+        icon: 'logos/github'
+    },
+]
